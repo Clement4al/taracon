e1f1dedda8f928301861175b8d3d3270cf6fb61d
clement4al <michaelokonclement@gmail.com>
2025-07-30 08:11:52 +0100

    feat: Registration form

---
+++ app/Livewire/CartCount.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class CartCount extends Component
+{
+
+    public function render()
+    {
+        return view('livewire.cart-count');
+    }
+}
---
+++ resources/views/livewire/cart-count.blade.php
@@ -0,0 +1,4 @@
+<tag x-data @cart-updated.window="$wire.call('$refresh')">
+    {{ cart()->products->sum() }}
+</tag>
+
